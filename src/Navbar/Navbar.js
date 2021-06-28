c781eb83be7f12ddb55f1995ac3111e40ac387f1
Alex Gomes <alex.gduguay@gmail.com>
2021-06-28 18:00:09 +0100

    Initila commit

---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`navbar ${show && "navbar__black"}`}>
+      <img
+        className="navbar__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png"
+        alt="Netflix Logo"
+      />
+    </div>
+  );
+};
+
+export default Navbar;
